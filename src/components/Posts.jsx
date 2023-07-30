89c67fbdc71cbe6c6751aaf62ca0390574f7c680
hendellcosta <discordstests@gmail.com>
2023-07-30 22:38:15 +0100

    - save old posts code

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -122,3 +122,96 @@ function Posts() {
 }
 
 export default Posts;
+
+{/**  import user1 from "../assets/user1.jpg";
+//import user2 from "../assets/user2.jpg";
+
+import {BiMessageRounded, BiRepost} from 'react-icons/bi'
+import {AiOutlineHeart, AiFillHeart} from 'react-icons/ai'
+import {LuShare} from 'react-icons/lu'
+import { useState } from "react";
+
+function Posts() {
+  const [isClicked, setIsClicked] = useState(false);
+  const [likes, setLikes] = useState(10);
+  
+  const [posts, setPosts] = useState([
+    {
+      image: user1,
+      name: 'Devon Lane',
+      time: '23s',
+      comments: 10,
+      reposts: 10,
+      likes: 10,
+      shares: 10,
+      content: "I interviewed over 70 back-end engineers. I created a Notion based Learn Backend Development Pack V2 that contains a learning path and practice tasks to become a backend dev based on ONLY FREE resources. Retweet and reply with free and I'll DM it to you. (need to follow)"
+    },
+    {
+      image: user1,
+      name: 'Devon Lane',
+      time: '1m',
+      comments: 10,
+      reposts: 10,
+      likes: 10,
+      shares: 10,
+      content: "I interviewed over 70 back-end engineers. I created a Notion based Learn Backend Development Pack V2 that contains a learning path and practice tasks to become a backend dev based on ONLY FREE resources. Retweet and reply with free and I'll DM it to you. (need to follow)"
+    },
+    {
+      image: user1,
+      name: 'Devon Lane',
+      time: '5h',
+      comments: 10,
+      reposts: 10,
+      likes: 10,
+      shares: 10,
+      content: "I interviewed over 70 back-end engineers. I created a Notion based Learn Backend Development Pack V2 that contains a learning path and practice tasks to become a backend dev based on ONLY FREE resources. Retweet and reply with free and I'll DM it to you. (need to follow)"
+    },
+  ])
+
+  setPosts;
+
+  const handleLikes = () => {
+    setIsClicked(true);
+    setLikes(likes + 1)
+  }
+
+  const handleRemoveLikes = () => {
+    setIsClicked(false);
+    setLikes(likes - 1)
+  }
+
+  return (
+    <div className="flex flex-col py-4 border-y-2 border-slate-600 border-spacing-4 divide-y-2 divide-slate-600 px-10"> */}
+      {/* ONE POST */}
+
+  {/*}
+      {posts.map((p, index) => (
+      <div className="pt-8" key={index}>
+        <div className="flex flex-row items-center gap-3 text-white text-xl">
+          <img src={p.image} alt="Twitter Profile Picture" className="rounded-full" />
+          <h1 className="font-bold">{p.name}</h1>
+          <p className="text-slate-500">@{p.name.replace(/[ ]/g, '')}</p>
+          <p className="text-slate-500">•</p>
+          <p className="text-slate-500">{p.time}</p>
+        </div>
+
+        <div className="px-20 text-2xl">
+          <p>{p.content}</p>
+        </div>
+
+        <div className="flex flex-row justify-between items-center pl-20 pr-32 text-2xl text-slate-400">
+            <p className="cursor-pointer flex flex-row items-center gap-3 py-10"><BiMessageRounded /> {p.comments}</p>
+            <p className="cursor-pointer flex flex-row items-center gap-3 py-10"><BiRepost /> {p.reposts}</p>
+            <p className={`${isClicked ? 'text-pink-600' : ''} cursor-pointer flex flex-row items-center gap-3 py-10`} onClick={!isClicked ? handleLikes : handleRemoveLikes}>{isClicked ? <AiFillHeart /> : <AiOutlineHeart />} {likes}</p>
+            <p className="cursor-pointer flex flex-row items-center gap-3 py-10"><LuShare /> {p.shares}</p>
+        </div>
+      </div>
+      ))}
+
+    </div>
+  );
+}
+
+export default Posts;
+
+*/}
